39f68b6b6db3fd757ebc3460d6e804c949537d48
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-03-18 09:20:30 -0300

    Tratamento de excecoes

---
+++ Curso Udemy/Secao 11/Exercicio11_1/Exercicio11_1/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Exercicio11_1.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException (string message) : base(message)
+        {
+
+        }
+    }
+}
